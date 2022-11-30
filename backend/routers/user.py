732211e464735e1896470c9ae0d25c3a9ce1cc2d
Justus188 <justus_ng@yahoo.com.sg>
2022-12-01 01:47:20 +0800

    Update readme

--- backend/routers/user.py
+++ backend/routers/user.py
@@ -6,7 +6,7 @@ from database import get_db
 import schemas, models
 from auth import get_password_hash, get_user_by_token
 import util.user_checks as checks
-from util.common import responses
+from util import common
 
 def hash_request(request: schemas.UserCreate|dict) -> dict:
     if type(request) is dict: request_dict = request.copy()
@@ -30,18 +30,18 @@ def create_user(request: schemas.UserCreate, db: Session = Depends(get_db)):
     return new_user
 
 @router.get('/', response_model = List[schemas.UserRead],
-    responses = responses['unauthenticated'] | checks.responses['admin'])
+    responses = common.responses['unauthenticated'] | checks.responses['admin'])
 def get_all_users(db: Session = Depends(get_db), user: models.User = Depends(get_user_by_token)): #limit: int = 10, skip: int = 0): # Pagination
     checks.admin(user)
     return db.query(models.User).all() #.offset(skip).limit(limit).all() # Pagination
 
 @router.get('/me', response_model = schemas.UserRead,
-    responses = responses['unauthenticated'])
+    responses = common.responses['unauthenticated'])
 def get_user_me(user: models.User = Depends(get_user_by_token)):
     return user
 
 @router.get('/{id}', response_model = schemas.UserRead,
-    responses = responses['unauthenticated'] | checks.responses['exists'] | checks.responses['me_or_admin'])
+    responses = common.responses['unauthenticated'] | checks.responses['exists'] | checks.responses['me_or_admin'])
 def get_user_by_id(id: int, db: Session = Depends(get_db), user: models.User = Depends(get_user_by_token)):
     target = db.query(models.User).get(id)
     checks.exists_user(target)
@@ -49,7 +49,7 @@ def get_user_by_id(id: int, db: Session = Depends(get_db), user: models.User = D
     return target
 
 @router.put('/{id}', response_model = schemas.UserRead,
-    responses = responses['unauthenticated'] | checks.responses['exists'] | checks.responses['me_or_admin'] | checks.responses['duplicate'])
+    responses = common.responses['unauthenticated'] | checks.responses['exists'] | checks.responses['me_or_admin'] | checks.responses['duplicate'])
 def update_user(id: int, request: schemas.UserUpdate, db: Session = Depends(get_db), user: models.User = Depends(get_user_by_token)):
     dbUsers = db.query(models.User)
     querySet = dbUsers.filter(models.User.id == id)
@@ -64,7 +64,7 @@ def update_user(id: int, request: schemas.UserUpdate, db: Session = Depends(get_
     return target
 
 @router.delete('/{id}', status_code = status.HTTP_204_NO_CONTENT,
-    responses = responses['unauthenticated'] | checks.responses['exists'] | checks.responses['me_or_admin'])
+    responses = common.responses['unauthenticated'] | checks.responses['exists'] | checks.responses['me_or_admin'])
 def delete_user(id: int, db: Session = Depends(get_db), user: models.User = Depends(get_user_by_token)):
     target = db.query(models.User).get(id)
     checks.exists_user(target)
